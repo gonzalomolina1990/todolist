15f2a8302c9260f15dc8cdee01b68f327180619d
Gonzalo Molina <gonzalomolina1990@gmail.com>
2023-01-09 20:32:41 -0300

    components

---
+++ front/components/Home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Button, Table} from 'react-bootstrap';
+import 'boostrap/dist/css/bootstrap.min.css';
+import Todos from './to-dos'
+
+
+function Home(){
+    <div>
+        <div style={(margin="10rem")}>
+            <Table stripped bordererd hover size="sm">
+                <thead>
+                    <tr>
+                        <th>
+                            Task
+                        </th>
+                        <th>
+                            Responsible
+                        </th>
+                        <th>
+                            Date
+                        </th>
+                        <th>
+                            Completed
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Todos && Todos.length > 0 ?
+                        Todos.map((item) => {
+                            return(
+                                <tr>
+                                    <td>item.task</td>
+
+                                    <td>item.responsible</td>
+
+                                    <td>item.date</td>
+
+                                    <td>item.completed</td>
+                                </tr>
+                            )
+                        })
+                        : "No data available"
+                    } 
+                </tbody>
+            
+            </Table>
+        </div>
+    </div>
+}
---
+++ front/components/to-dos.js
@@ -0,0 +1,25 @@
+const Todos = [
+    {
+        id: "1",
+        task: "Comer",
+        responsible: "Juan",
+        date: "09/01/2023",
+        completed: "true"  
+    },
+    {
+        id: "2",
+        task: "Bailar",
+        responsible: "Pedro",
+        date: "10/01/2023",
+        completed: "false"   
+    },
+    {
+        id: "3",
+        task: "Jugar",
+        responsible: "Maria",
+        date: "09/01/2023",
+        completed: "true"   
+    },
+]
+
+export default Todos;
